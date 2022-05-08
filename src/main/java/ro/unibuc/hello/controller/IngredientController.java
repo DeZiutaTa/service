493540e2b154ccc53fa9d419022316117e0af57b
urseilinca <63130418+urseilinca@users.noreply.github.com>
2022-05-08 15:55:26 +0300

    Update IngredientController.java

--- src/main/java/ro/unibuc/hello/controller/IngredientController.java
+++ src/main/java/ro/unibuc/hello/controller/IngredientController.java
@@ -11,6 +11,10 @@ import ro.unibuc.hello.dto.AddIngredientDto;
 import ro.unibuc.hello.dto.IngredientDto;
 import ro.unibuc.hello.exception.BadRequestException;
 import ro.unibuc.hello.exception.NotFoundException;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.concurrent.atomic.AtomicLong;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,11 +25,17 @@ public class IngredientController {
 
     @Autowired
     private IngredientRepository ingredientRepository;
+    
+    @Autowired
+    MeterRegistry metricsRegistry;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/ingredient")
+    @Timed(value = "show.ingredient.time", description = "Time taken to return ingredient")
+    @Counted(value = "show.ingredient.count", description = "Times ingredient was returned")
     @ResponseBody
     public IngredientDto getIngredient(@RequestParam(name="name") String name) {
+        metricsRegistry.counter("my_non_aop_metric", "endpoint", "ingredient").increment(counter.incrementAndGet());
         var entity = ingredientRepository.findByName(name);
         if(entity == null) {
             throw new NotFoundException();
@@ -34,9 +44,12 @@ public class IngredientController {
     }
 
     @GetMapping("/ingredients")
+    @Timed(value = "show.ingredients.time", description = "Time taken to return all ingredients")
+    @Counted(value = "show.ingredients.count", description = "Times list of ingredients was returned")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List<IngredientDto> getAllIngredients() {
+        metricsRegistry.counter("my_non_aop_metric", "endpoint", "ingredients").increment(counter.incrementAndGet());
         var entities = ingredientRepository.findAll();
         if (entities.size() == 0) {
             throw new NotFoundException();
@@ -48,8 +61,11 @@ public class IngredientController {
     }
 
     @PostMapping("/ingredient/add")
+     @Timed(value = "add.ingredient.time", description = "Time taken to add ingredient")
+    @Counted(value = "add.ingredient.count", description = "Times an ingredient was added")
     @ResponseStatus(HttpStatus.CREATED)
     public void addIngredient(@RequestBody AddIngredientDto model) {
+        metricsRegistry.counter("my_non_aop_metric", "endpoint", "add/ingredient").increment(counter.incrementAndGet());
 
         if (model.price < 0) {
             throw new BadRequestException(new HashMap<>() {{
